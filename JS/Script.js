8ac84d5967c35a1357b44c5414f683079287b1a3
SandunLB <sandunlb2001@gmail.com>
2024-03-22 11:08:51 +0530

    Display elements and variables Scripted!

--- JS/Script.js
+++ JS/Script.js
@@ -16,3 +16,23 @@ const foodItemsArray = [
   '🥚',
 ];
 
+// game display elements
+const grid = document.querySelector('.grid');
+const scoreDisplay = document.querySelector('span');
+const startBtn = document.querySelector('.start-btn');
+const keyBtns = document.querySelectorAll('.keys-container button');
+
+// game variables
+const width = 10;
+const numCells = width * width;
+let currentSnake = [2, 1, 0];
+let snakeColor = Math.floor(Math.random() * 360);
+let snakeColorIncrement = 10;
+let direction = 1;
+let interval = 0;
+
+
+
+grid.style.width = `${width * 10 * 2}px`;
+grid.style.height = `${width * 10 * 2}px`;
+
